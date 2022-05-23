d59d98458a77d9ab0c5f426ff1eb32c387a92c4b
Araecy <74868163+Araecy@users.noreply.github.com>
2022-05-23 17:11:04 +0200

    update

--- index.php
+++ index.php
@@ -6,10 +6,26 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/index.min.css">
     <title>Filla te koop</title>
 </head>
 
 <body>
+
+<!-- nav -->
+
+<nav>
+    home
+    contact
+</nav>
+
+<!-- wave -->
+<div class="custom-shape-divider-top-1653318248">
+    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+        <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
+    </svg>
+</div>
+
 <div class="container my-12 mx-auto px-4 md:px-12">
     <div class="flex flex-wrap -mx-1 lg:-mx-4">
 
