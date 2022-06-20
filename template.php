6aff442df82674a79e884f2e920eb39c6b6d4284
Sennevn <87266@glr.nl>
2022-06-20 15:48:01 +0200

    template

---
+++ template.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/index.min.css">
+    <script src="./js/nav.js"></script>
+    <title>Template</title>
+</head>
+<body>
+<div id="img-template-villa-box"><img src="#" id="img-template-villa"></div>
+   <div id="bieden"> bieden</div>
+   <div id="beschrijving_villa"><h2 id="titel_villa">title</h2><br><p id="beschrijving_villa_vol">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam officia quis rem recusandae amet fugiat saepe nostrum odio voluptas soluta quod, at sit cum veniam dicta? Veniam magni quia incidunt corporis architecto. Quod nihil ex error aperiam facere iste beatae obcaecati. Hic architecto et neque autem fugit, cum nobis ipsum nam alias. Fuga nesciunt magni tempora, facere ipsum, hic possimus recusandae fugit ad sint commodi, voluptatibus obcaecati optio corporis laboriosam impedit sunt a officiis alias. Ex, facilis sint commodi sit minus cumque. Adipisci fuga labore itaque culpa accusamus fugit amet deserunt nesciunt! Veniam, amet magni quasi repellat harum dolor quisquam.</p></div>
+</body>
+</html>
